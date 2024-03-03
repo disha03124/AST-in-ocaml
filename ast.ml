0ccbfb0a0b115b54b841e6fdc159c640ae3764ab
disha <148572672+disha03124@users.noreply.github.com>
2024-03-03 07:28:08 +0530

    Update ast.ml

--- ast.ml
+++ ast.ml
@@ -8,16 +8,24 @@ type predicate_symbol = string
 type term =
   | Var of variable
   | Const of constant
-  | Func of function_symbol * term list
+  | Int of int
+  | Func of function_symbol * (term list)
+  | Empty 
+  | Non_Empty 
+  
+type term_list = Terms of term list
 
-type atomic_formula = predicate_symbol * term list
+type atomic_formula = Atm_form of predicate_symbol * (term list)
 
-type fact = atomic_formula
+(* type atomic_formula_list = Atm_formulas of atomic_formula list *)
 
-type rule = atomic_formula * atomic_formula list
+type fact = Fact of atomic_formula
+
+type rule = Rule of atomic_formula * (atomic_formula list)
 
 type clause =
   | Fact of fact
   | Rule of rule
 
-type goal = atomic_formula list
+type goal = Goal of (atomic_formula list)
+type clause_list = Clause_list of clause list
