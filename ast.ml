c97720415ded297129ad84b33ea6bf946b35ce74
disha <148572672+disha03124@users.noreply.github.com>
2024-03-03 15:25:33 +0530

    Update ast.ml

--- ast.ml
+++ ast.ml
@@ -10,17 +10,20 @@ type keyword = string
 
 
 
+
 type term =
   | Var of variable
   | Const of constant
   | Int of int
   | Func of function_symbol * (term list)
   | Empty 
-  (* | Key_word of string *)
+  | Key_word of keyword
   | Underscore of char
   | Identifier of identifier
   | Non_Empty 
   | MoreTerm of term * term 
+  (* | Equal of char
+  | Semicolon of char *)
   
 type term_list = Terms of term list
 
@@ -29,6 +32,11 @@ type atomic_formula =
       | Underscore of char
       | Cut of char
       | Key_word of keyword
+      | Var of variable
+      | Identifier of identifier
+      | Non_Empty
+      (* | Equal of char
+      | Semicolon of char *)
 
 (* type atomic_formula_list = Atm_formulas of atomic_formula list *)
 
