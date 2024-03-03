927fb231a0c1c193f2fd7387005bc3dbd48f352a
disha <148572672+disha03124@users.noreply.github.com>
2024-03-03 11:50:30 +0530

    Update main.ml

--- main.ml
+++ main.ml
@@ -2,39 +2,27 @@ open Ast
 open Parser
 open Lexing
 
+let parse_with_error lexbuf =
+  try
+    Parser.program Lexer.token lexbuf
+  with
+  | Parsing.Parse_error ->
+      let pos = lexbuf.Lexing.lex_curr_p in
+      let line = pos.Lexing.pos_lnum in
+      let col = pos.Lexing.pos_cnum - pos.Lexing.pos_bol in
+      Printf.printf "Syntax error at line %d, column %d\n" line col;
+      exit 1
+
 (* Function to parse input string *)
 let parse_string input =
   let lexbuf = Lexing.from_string input in
-  Parser.program Lexer.token lexbuf
+  try
+    parse_with_error lexbuf
+  with
+  | Exit -> failwith "Parsing failed"  (* Removed () argument *)
 
 (* Sample input *)
-let input = "apple(Red, Sweet):- hello(H, I), straw(90)."
-
-(* Define string conversion functions *)
-(* let rec string_of_term term =
-  match term with
-  | Var v -> v
-  | Const c -> c
-  | Func (f, args) ->
-    let args_str = String.concat ", " (List.map string_of_term args) in
-    Printf.sprintf "%s(%s)" f args_str
-
-let string_of_term_list terms =
-  String.concat ", " (List.map string_of_term terms)
-
-let string_of_atomic_formula (predicate, terms) =
-  let terms_str = string_of_term_list terms in
-  Printf.sprintf "%s(%s)" predicate terms_str *)
+let input = " hello(H,as):-  hii(True).\n fun(9). \n ?-fun(10)."
 
 (* Parse the input *)
 let clauses = parse_string input
-
-(* Display the parsed clauses *)
-(* let () =
-  List.iter (function
-    | Fact f -> Printf.printf "Fact: %s\n" (string_of_atomic_formula f)
-    | Rule (head, body) -> 
-        let head_str = string_of_atomic_formula head in
-        let body_str = String.concat ", " (List.map string_of_atomic_formula body) in
-        Printf.printf "Rule: %s :- %s\n" head_str body_str
-  ) clauses *)
