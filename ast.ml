78e6471ff2897f4d41533047345dd98309101885
disha <148572672+disha03124@users.noreply.github.com>
2024-03-03 11:26:10 +0530

    Update ast.ml

--- ast.ml
+++ ast.ml
@@ -3,7 +3,11 @@
 type variable = string
 type constant = string
 type function_symbol = string
-type predicate_symbol = string
+(* type predicate_symbol = string *)
+type keyword = string
+type identifier = string
+
+
 
 type term =
   | Var of variable
@@ -11,21 +15,33 @@ type term =
   | Int of int
   | Func of function_symbol * (term list)
   | Empty 
+  | Key_word of keyword
+  | Underscore of char
+  | Identifier of identifier
   | Non_Empty 
+  | MoreTerm of term * term 
   
 type term_list = Terms of term list
 
-type atomic_formula = Atm_form of predicate_symbol * (term list)
+type atomic_formula = 
+      Atm_form of function_symbol * (term list)
+      | Underscore of char
+      | Cut of char
 
 (* type atomic_formula_list = Atm_formulas of atomic_formula list *)
 
 type fact = Fact of atomic_formula
 
-type rule = Rule of atomic_formula * (atomic_formula list)
+type rule = 
+    Rule of atomic_formula * (atomic_formula list)
+  | Truth of atomic_formula * char
 
 type clause =
   | Fact of fact
   | Rule of rule
+  | Truth of rule
 
-type goal = Goal of (atomic_formula list)
+type goal = atomic_formula list
+
 type clause_list = Clause_list of clause list
+  | Prog of (clause list) * goal
